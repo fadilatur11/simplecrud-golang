2649fcae65ca2687d9dc148c56f6747d7e12d501
fadilatur11 <fadhilatur7@gmail.com>
2021-02-02 13:25:42 +0700

    setup golang

---
+++ go.mod
@@ -0,0 +1,10 @@
+module go-crud.com
+
+go 1.15
+
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/jinzhu/gorm v1.9.16
+	gorm.io/driver/mysql v1.0.4
+	gorm.io/gorm v1.20.12
+)
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"go-crud.com/controllers"
+	"go-crud.com/models"
+)
+
+func main() {
+	models.ConnectDatabase()
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"data": "hello world"})
+	})
+	r.GET("/books", controllers.FindBooks)
+	r.POST("/books", controllers.CreateBook)
+
+	r.Run()
+}
